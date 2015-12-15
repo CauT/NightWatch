88527bc81a1bfcdf2ffc37506641b2cb427ddd32
CauT <donglingyongadls@gmail.com>
2015-12-15 16:06:17 +0800

    reindent, android.js achieve DrawerLayout

--- index.android.js
+++ index.android.js
@@ -6,47 +6,79 @@
 
 var React = require('react-native');
 var {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
+    AppRegistry,
+    StyleSheet,
+    Dimensions,
+    DrawerLayoutAndroid,
+    Text,
+    View,
 } = React;
 
+var DRAWER_WIDTH_LEFT = 56;
+
 var NightWatch = React.createClass({
-  render: function() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
+    render: function() {
+        /*
+        return (
+                <View style={styles.container}>
+                <Text style={styles.welcome}>
+                Welcome to React Native!
+                </Text>
+                <Text style={styles.instructions}>
+                To get started, edit index.android.js
+                </Text>
+                <Text style={styles.instructions}>
+                Shake or press menu button for dev menu
+                </Text>
+                </View>
+               );
+               */
+        return (
+            <DrawerLayoutAndroid
+            drawerPosition={DrawerLayoutAndroid.positions.Left}
+            drawerWidth={Dimensions.get('window').width - DRAWER_WIDTH_LEFT}
+            keyboardDismissMode="on-drag"
+            ref={(drawer) => { this.drawer = drawer; }}
+            renderNavigationView={this._renderNavigationView}
+            >
+            </DrawerLayoutAndroid>
+        );
+    },
+
+    _renderNavigationView: function() {
+        return (
+                <View style={styles.container}>
+                <Text style={styles.welcome}>
+                Welcome to React Native!
+                </Text>
+                <Text style={styles.instructions}>
+                To get started, edit index.android.js
+                </Text>
+                <Text style={styles.instructions}>
+                Shake or press menu button for dev menu
+                </Text>
+                </View>
+               );
+    },
 });
 
 var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
 });
 
 AppRegistry.registerComponent('NightWatch', () => NightWatch);
--- index.ios.js
+++ index.ios.js
@@ -6,48 +6,48 @@
 
 var React = require('react-native');
 var {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
 } = React;
 
 var NightWatch = React.createClass({
-  render: function() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
+    render: function() {
+        return (
+                <View style={styles.container}>
+                <Text style={styles.welcome}>
+                Welcome to React Native!
+                </Text>
+                <Text style={styles.instructions}>
+                To get started, edit index.ios.js
+                </Text>
+                <Text style={styles.instructions}>
+                Press Cmd+R to reload,{'\n'}
+                Cmd+D or shake for dev menu
+                </Text>
+                </View>
+               );
+    }
 });
 
 var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
 });
 
 AppRegistry.registerComponent('NightWatch', () => NightWatch);

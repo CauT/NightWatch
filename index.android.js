09b98f7112ca6725cf9a3cf6bbae21a28360841e
CauT <donglingyongadls@gmail.com>
2015-12-26 15:22:30 +0800

    add the hightlight of pressing button

--- index.android.js
+++ index.android.js
@@ -13,89 +13,173 @@ var {
     Text,
     View,
     ListView,
+    Image,
+    PixelRatio,
+    TouchableHighlight,
 } = React;
 
-var DRAWER_WIDTH_LEFT = 280;
-var majorFuncNames = ['土壤墒情', '气象信息', '生长过程监测', '可信溯源', '病虫害监测']
+var DRAWER_WIDTH_LEFT = 42;
+
+var majorItemInfos = [
+    {
+        name: '土壤墒情',
+        icon: require('image!icon_sapling'),
+    },
+    {
+        name: '气象信息',
+        icon: require('image!icon_weather'),
+    },
+    {
+        name: '生长过程监测',
+        icon: require('image!icon_chart'),
+    },
+    {
+        name: '可信溯源',
+        icon: require('image!icon_certificate'),
+    },
+    {
+        name: '病虫害监测',
+        icon: require('image!icon_microscope'),
+    },
+];
+
+var settingItemInfos = [
+    {
+        name: '账户信息',
+        icon: require('image!icon_account'),
+    },
+    {
+        name: '应用设置',
+        icon: require('image!icon_settings')
+    },
+];
+
 var appName = '农业监测终端';
+
 
 var NightWatch = React.createClass({
     getInitialState: function() {
         var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-        /* testing sticky
-        for (var i=0; i<20; i++) {
-            majorFuncNames.push('Hello World!')
+                /* testing sticky
+        for (var i=0; i<42; i++) {
+            majorItemNames.push('Hello World!')
         }
-        */
+                */
         return {
-            dataSource: ds.cloneWithRows(majorFuncNames),
+            majorDataSource: ds.cloneWithRows(majorItemInfos),
+            settingDataSource: ds.cloneWithRows(settingItemInfos),
         };
     },
     render: function() {
         return (
-                   <DrawerLayoutAndroid
-                   drawerPosition={DrawerLayoutAndroid.positions.Left}
-                   drawerWidth={Dimensions.get('window').width - DRAWER_WIDTH_LEFT}
-                   keyboardDismissMode="on-drag"
-                   ref={(drawer) => { this.drawer = drawer; }}
-                   renderNavigationView={this._renderNavigationView}
-                   style={styles.sideBar}
-                   >
-                   </DrawerLayoutAndroid>
-               );
+            <DrawerLayoutAndroid
+                drawerPosition={DrawerLayoutAndroid.positions.Left}
+                drawerWidth={Dimensions.get('window').width
+                    - DRAWER_WIDTH_LEFT * PixelRatio.get()}
+                keyboardDismissMode="on-drag"
+                renderNavigationView = {this._renderNavigationView}
+                ref={(drawer) => { this.drawer = drawer; }}
+                style={styles.sideBar}
+            >
+            </DrawerLayoutAndroid>
+        );
     },
 
     _renderNavigationView: function() {
         return (
-                <ListView
-                style={styles.sideBar}
-                dataSource={this.state.dataSource}
-                renderSectionHeader={this._renderSectionHeader}
-                renderRow={
-                    (rowData) => <Text style={styles.majorFunc}>
-                        {rowData}
+            <View style={styles.sideBar}>
+                <View>
+                    <Text style={styles.sideBarTitle}>
+                        {appName}
                     </Text>
-                }
+                </View>
+                <View style={styles.whileLine}/>
+                <ListView style={styles.listContainer}
+                    dataSource={this.state.majorDataSource}
+                    renderRow={
+                        (rowData) => <TouchableHighlight>
+                            <View style={styles.majorItem}>
+                                <Image
+                                    style={styles.majorItemIcon}
+                                    source={rowData.icon}/>
+                                <Text style={styles.majorItemName}>
+                                    {rowData.name}
+                                </Text>
+                            </View>
+                        </TouchableHighlight>
+                    }
                 />
-               );
+                <View style={styles.whileLine}/>
+                <ListView style={styles.listContainer}
+                    dataSource={this.state.settingDataSource}
+                    renderRow={
+                        (rowData) => <TouchableHighlight>
+                            <View style={styles.majorItem}>
+                                <Image
+                                    style={styles.majorItemIcon}
+                                    source={rowData.icon}/>
+                                <Text style={styles.majorItemName}>
+                                    {rowData.name}
+                                </Text>
+                            </View>
+                        </TouchableHighlight>
+                    }
+                />
+                <View style={styles.listRest}/>
+            </View>
+        );
     },
 
     _renderSectionHeader: function() {
         return (
+                <View>
                 <Text style={styles.sideBarTitle}>{appName}</Text>
-                );
+                </View>
+        );
     }
 });
 
 var styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
+    majorItem: {
+        justifyContent: 'flex-start',
+        flexDirection: 'row',
     },
-    welcome: {
+    majorItemName: {
         fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
+        marginTop: 8,
+        marginBottom: 8,
+        marginLeft: 12,
+        color: '#FFFFFF',
     },
-    instructions: {
-        textAlign: 'center',
-        color: '#333333',
-        marginBottom: 5,
-    },
-    majorFunc: {
-        fontSize: 20,
+    majorItemIcon: {
+        width: 20,
+        height: 20,
+        marginTop: 10,
         marginBottom: 10,
-        color: '#000000',
+        marginLeft: 12,
     },
     sideBar: {
-        backgroundColor: '#F5FCFF',
+        flex: 1,
+        backgroundColor: '#1A1921',
     },
     sideBarTitle: {
         fontSize: 30,
+        marginLeft: 12,
+        marginTop: 20,
+        color: '#FFFFFF',
+    },
+    whileLine: {
+        backgroundColor: '#FFFFFF',
+        marginTop: 10,
         marginBottom: 10,
-        color: '#000000',
+        marginHorizontal: 5,
+        height: 1 / PixelRatio.get(),
+    },
+    listContainer: {
+        flex: 0,
+    },
+    listRest: {
+        flex: 1,
     }
 });
 

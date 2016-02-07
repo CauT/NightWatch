285daa92562c53028308b56ea58185f07c2d8405
CauT <donglingyongadls@gmail.com>
2016-02-07 09:14:16 +0800

    achieve increasing number annimation

--- VectorWidget.js
+++ VectorWidget.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 var React = require('react-native');
 
@@ -12,68 +12,62 @@ var AnimatedText = createAnimatedTextComponent();
 var AnimatedProps = Animated.__PropsOnlyForTests;
 
 function createAnimatedTextComponent() {
-  var refName = 'node';
+    var refName = 'node';
 
-  class AnimatedComponent extends React.Component {
-    _propsAnimated: AnimatedProps;
+    class AnimatedComponent extends React.Component {
+        _propsAnimated: AnimatedProps;
 
-    componentWillUnmount() {
-      this._propsAnimated && this._propsAnimated.__detach();
+        componentWillUnmount() {
+            this._propsAnimated && this._propsAnimated.__detach();
+        }
+
+        setNativeProps(props) {
+            this.refs[refName].setNativeProps(props);
+        }
+
+        componentWillMount() {
+            this.attachProps(this.props);
+        }
+
+        attachProps(nextProps) {
+            var oldPropsAnimated = this._propsAnimated;
+
+            // The system is best designed when setNativeProps is implemented. It is
+            // able to avoid re-rendering and directly set the attributes that
+            // changed. However, setNativeProps can only be implemented on leaf
+            // native components. If you want to animate a composite component, you
+            // need to re-render it. In this case, we have a fallback that uses
+            // forceUpdate.
+            var callback = () => {
+                this.forceUpdate();
+            };
+
+            this._propsAnimated = new AnimatedProps(
+                nextProps,
+                callback,
+            );
+
+            oldPropsAnimated && oldPropsAnimated.__detach();
+        }
+
+        componentWillReceiveProps(nextProps) {
+            this.attachProps(nextProps);
+        }
+
+        render() {
+            var tmpText = this._propsAnimated.__getAnimatedValue().text;
+            return (
+                <Text
+                    {...this._propsAnimated.__getValue()}
+                    ref={refName}
+                >
+                    {Math.floor(tmpText)}
+                </Text>
+            );
+        }
     }
 
-    setNativeProps(props) {
-      this.refs[refName].setNativeProps(props);
-    }
-
-    componentWillMount() {
-      this.attachProps(this.props);
-    }
-
-    attachProps(nextProps) {
-      var oldPropsAnimated = this._propsAnimated;
-
-      // The system is best designed when setNativeProps is implemented. It is
-      // able to avoid re-rendering and directly set the attributes that
-      // changed. However, setNativeProps can only be implemented on leaf
-      // native components. If you want to animate a composite component, you
-      // need to re-render it. In this case, we have a fallback that uses
-      // forceUpdate.
-      var callback = () => {
-          this.forceUpdate();
-      }
-
-
-      this._propsAnimated = new AnimatedProps(
-        nextProps,
-        callback,
-      );
-
-      oldPropsAnimated && oldPropsAnimated.__detach();
-    }
-
-    componentWillReceiveProps(nextProps) {
-      this.attachProps(nextProps);
-    }
-
-    shouldComponentUpdate(nextProps, nextState) {
-        return (nextState.degree.__getValue() !== this.state.degree.__getValue())
-            && (nextState.degree.__getValue() === nextState.degree);
-    }
-
-    render() {
-        var tmpText = this._propsAnimated.__getAnimatedValue().text;
-      return (
-        <Text
-          {...this._propsAnimated.__getValue()}
-          ref={refName}
-        >
-            {Math.floor(tmpText)}
-        </Text>
-      );
-    }
-  }
-
-  return AnimatedComponent;
+    return AnimatedComponent;
 }
 
 var VectorWidget = React.createClass({
@@ -96,9 +90,9 @@ var VectorWidget = React.createClass({
 
     render: function() {
         return (
-                    <AnimatedText
-                        style={{fontSize: this.state.degree}}
-                        text={this.state.degree} />
+            <AnimatedText
+                style={{fontSize: this.state.degree}}
+                text={this.state.degree} />
         );
     }
 });

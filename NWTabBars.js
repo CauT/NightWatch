992c30c7ed83790a24dadf4c3e94466fd6f355dd
CauT <donglingyongadls@gmail.com>
2016-02-16 13:46:22 +0800

    remove soilItemInfos from NWTabBars.js to string.json

--- NWTabBars.js
+++ NWTabBars.js
@@ -11,20 +11,7 @@ var {
     PullToRefreshViewAndroid,
 } = React;
 
-var tabInfos = [
-    {
-        tabIcon: 'ion|ios-paper',
-        tabName: '当前数据',
-    },
-    {
-        tabIcon: 'ion|calendar',
-        tabName: '历史数据',
-    },
-    {
-        tabIcon: 'ion|connection-bars',
-        tabName: '图表生成',
-    },
-];
+var {soilItemInfos} = require('./string.json');
 
 var ScrollableTabView = require('react-native-scrollable-tab-view');
 var TabBar = require('./TabBar');
@@ -40,22 +27,14 @@ var NWTabBars = React.createClass({
         return (
             <View style={styles.container}>
                 <ScrollableTabView initialPage={1} renderTabBar={() => <TabBar />}>
-                    <PullToRefreshViewAndroid
-                        tabLabel={tabInfos[0]}
-                        style={styles.pullToRefreshLayout}
-                        refreshing={this.state.isRefreshing}
-                        onRefresh={this._onRefresh}
-                        colors={['#01A971']}
-                        progressBackgroundColor={'#FFFFFF'}
-                    >
-                        <ScrollView style={styles.tabView}>
-                                <Text>Friends</Text>
-                        </ScrollView>
-                    </PullToRefreshViewAndroid>
-                    <View tabLabel={tabInfos[1]} style={styles.tabView}>
+
+                    <ScrollView tabLabel={soilItemInfos[0]} style={styles.tabView}>
+                            <Text>Friends</Text>
+                    </ScrollView>
+                    <View tabLabel={soilItemInfos[1]} style={styles.tabView}>
                         <RisingNumber style={styles.card}/>
                     </View>
-                    <ScrollView tabLabel={tabInfos[2]} style={styles.tabView}>
+                    <ScrollView tabLabel={soilItemInfos[2]} style={styles.tabView}>
                         <View style={styles.card}>
                             <Text>Messenger</Text>
                         </View>
